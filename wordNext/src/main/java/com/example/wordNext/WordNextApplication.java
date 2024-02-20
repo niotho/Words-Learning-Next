46cc2f232c95b0a3910fe0a9755a149a23a639a2
niotho <dtnbah@gmail.com>
2024-02-20 14:01:24 +0000

    creating the project

---
+++ wordNext/src/main/java/com/example/wordNext/WordNextApplication.java
@@ -0,0 +1,13 @@
+package com.example.wordNext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordNextApplication.class, args);
+	}
+
+}
---
+++ wordNext/src/test/java/com/example/wordNext/WordNextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wordNext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordNextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
